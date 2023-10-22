9ae0bc084055c1b525bc121732564c98bb10d9b7
Luis181996 <luishumberto181996@hotmail.com>
2023-10-22 09:54:02 -0500

    primer commit

---
+++ src/main/java/cibertec/pe/edu/appcinecibertec/AppcinecibertecApplication.java
@@ -0,0 +1,13 @@
+package cibertec.pe.edu.appcinecibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcinecibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcinecibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/pe/edu/appcinecibertec/AppcinecibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.pe.edu.appcinecibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcinecibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
